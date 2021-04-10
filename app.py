b69e5de67a622cfda51f818f0073a5b1f555fa48
Kieran <kierantmcaleer@gmail.com>
2021-04-09 21:55:46 -0400

    got the first two routes working

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 
 import numpy as np
 import pandas as pd
-import datetime as dt
+import datetime as datetime
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
@@ -17,5 +17,44 @@ engine = create_engine("sqlite:///hawaii.sqlite")
 Base = automap_base()
 
 Base.prepare(engine, reflect=True)
-Measurement = Base.classes.measurement
 Station = Base.classes.station
+Measurement = Base.classes.measurement
+session = Session(engine)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    """Return results that have a date a a corresponding percipitation value"""
+    date = session.query(func.max(Measurement.date)).first()
+    year_ago =(datetime.datetime.strptime(date[0], '%Y-%m-%d') - datetime.timedelta(days=365)).strftime('%Y-%m-%d')
+    prcp_one_year = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date.between(year_ago,date[0])).all()
+    
+    # Convert list of tuples into a dict
+    precipitation_dict = dict(prcp_one_year)
+
+    return jsonify(precipitation_dict)
+
+@app.route("/api/v1.0/stations")
+def stations():
+    """Return a list of all passenger names"""
+    # Query all passengers
+    station_list= session.query(Measurement.station).all()
+  
+
+    return jsonify(station_list)
+
+
+if __name__ == '__main__':
+    app.run()
