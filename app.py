6ba3d6108867dac73d7700b2508bad6f007b852d
Kieran <kierantmcaleer@gmail.com>
2021-04-09 22:47:03 -0400

    Finished all of the routes

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ session = Session(engine)
 
 app = Flask(__name__)
 
-
+# Create the main route that shows you all of the others
 @app.route("/")
 def welcome():
     """List all available api routes."""
@@ -31,8 +31,11 @@ def welcome():
         f"Available Routes:<br/>"
         f"/api/v1.0/precipitation<br/>"
         f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end"
     )
+
 
 @app.route("/api/v1.0/precipitation")
 def precipitation():
@@ -44,16 +47,37 @@ def precipitation():
     # Convert list of tuples into a dict
     precipitation_dict = dict(prcp_one_year)
 
-    return jsonify(precipitation_dict)
+    return jsonify(precipitation_dict=precipitation_dict)
 
 @app.route("/api/v1.0/stations")
 def stations():
-    """Return a list of all passenger names"""
+    """Return a list of all Stations"""
     # Query all passengers
-    station_list= session.query(Measurement.station).all()
+    station_list= session.query(Measurement.station).group_by(Measurement.station).all()
   
 
-    return jsonify(station_list)
+    return jsonify(station_list=station_list)
+@app.route("/api/v1.0/tobs")
+def tobs():
+    """Return a list of measurements from most active station over the past year"""
+    # Query all passengers
+    date = session.query(func.max(Measurement.date)).first()
+    year_ago =(datetime.datetime.strptime(date[0], '%Y-%m-%d') - datetime.timedelta(days=365)).strftime('%Y-%m-%d')
+    temp_one_year = session.query(Measurement.date, Measurement.tobs).filter(Measurement.date.between(year_ago,date[0])).all()
+    return jsonify(temp_one_year=temp_one_year)
+
+@app.route("/api/v1.0/<start>")
+def start(start=None):
+    """Returns minimum, maximum, and average temp after a start ramge"""
+    summary = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start).all()
+    
+    return jsonify(summary = summary)
+@app.route("/api/v1.0/<start>/<end>")
+def startend(start=None, end=None):
+    """Returns minimum, maximum, and average temp between a date range"""
+    summary_both = session.query(func.min(Measurement.tobs),func.max(Measurement.tobs),func.avg(Measurement.tobs)).filter(Measurement.date >= start).filter(Measurement.date <= end).all()
+    
+    return jsonify(summary_both = summary_both)
 
 
 if __name__ == '__main__':
